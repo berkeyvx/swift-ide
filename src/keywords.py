535be42424ac97a03b10b887cbaf459c483ee5f7
Berke Yavas <iamberkeyavas@gmail.com>
2020-12-18 23:35:16 +0300

    keywords.py contains swift keyword class

---
+++ src/keywords.py
@@ -0,0 +1,12 @@
+
+
+class SwiftKeywords(object):
+    def __init__(self):
+        self.keyword_list = {"break", "case", "continue", "default",
+                             "do", "else", "fallthrough", "for",
+                             "if", "in", "return", "switch", "where", "while"}
+        
+    def is_keyword(self, word):
+        return (word in self.keyword_list)
+        
+    
